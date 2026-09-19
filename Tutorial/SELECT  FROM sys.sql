81b9891bb07dce66255aaafa5740945c758a566e
Hadi <143005529+HadiSeyed@users.noreply.github.com>
2026-09-19 15:51:09 -0400

    Tutorial

---
+++ Tutorial/SELECT  FROM sys.sql
@@ -0,0 +1,8 @@
+SELECT * FROM sys.database_files;
+
+-- Run it
+SELECT * From sys.databases
+-- Execute it
+SELECT * From sys.schemas
+-- Execute it in the "sql-server-overview.md" file
+SELECT * From sys.dm_exec_requests
