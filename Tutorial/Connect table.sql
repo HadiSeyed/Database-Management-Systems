e9e6112b3babcd764799fc23d7bd77f56925b903
Hadi <143005529+HadiSeyed@users.noreply.github.com>
2026-09-19 23:35:10 -0400

    Tutorial

---
+++ Tutorial/Connect table.sql
@@ -0,0 +1,7 @@
+select UniversityName, CollegeName, DepartmentName
+
+from University u
+    inner join College col on u.UniversityId = col.UniversityId
+    inner join Department d on col.CollegeId = d.CollegeId
+    inner join Program p on d.DepartmentId = p.DepartmentId
+    inner join Course c on p.ProgramId = c.ProgramId
