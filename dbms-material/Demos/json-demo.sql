f038eb2e84a6d2cb27135a03bb41797e9d10be07
Hadi <143005529+HadiSeyed@users.noreply.github.com>
2026-09-23 23:26:42 -0400

    Course Materials

---
+++ dbms-material/Demos/json-demo.sql
@@ -0,0 +1,12 @@
+
+-- Lecture Sep 21
+
+-- -- Change the database (location):
+-- 1. change database totally: 
+-- use the button at the top right or in the bottom right to switch it.
+-- 2. change only in that script:
+-- use YourDatabaseName;
+-- Go
+
+use UniversityDocumentDB;
+Go
--- dbms-material/Demos/select-statement.sql
+++ dbms-material/Demos/select-statement.sql
@@ -1,7 +1,7 @@
 
 -- Lecture Sep 16
 
--- -- Change the database:
+-- -- Change the database (location):
 -- 1. change database totally: 
 -- use the button at the top right or in the bottom right to switch it.
 -- 2. change only in that script:
